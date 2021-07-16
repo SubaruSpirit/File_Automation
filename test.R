69772a4d69f9d268b75a49efc50bfae5f3287479
SubaruSpirit <73080475+SubaruSpirit@users.noreply.github.com>
2021-07-16 11:04:40 +0100

    Add files via upload

--- test.R
+++ test.R
@@ -25,7 +25,10 @@ ui <- shinyUI(fluidPage(
       textInput("date", "Date"),
       selectInput("vial_kit", label = "Kit or Vial?",
                   choices = c("Kit","Vial",""), selected = ""),
-      actionButton("approve","Approve")
+      actionButton("approve","Approve"),
+      textOutput("feedback"),
+      actionButton("next_pdf","Next PDF"),
+      textOutput("end")
     ),
     
     mainPanel(
@@ -40,19 +43,21 @@ server <- shinyServer(function(input, output, session) {
   # table of the imported file
   output$files <- renderTable({input$file_import})
   
-### display the pdf ########################################################
-  observe({
-    req(input$file_import)
+  ### display the pdf ########################################################
+  x = reactiveVal(1)
+  
+  observeEvent(input$file_import,{
     
-    file.copy(input$file_import$datapath,"www", overwrite = T)
+    file.rename(input$file_import$datapath[x()], "0.pdf")
+    file.copy("0.pdf","www", overwrite = T)
     
     output$pdfview <- renderUI({
       tags$iframe(style="height:1200px; width:100%", src="0.pdf")
     })
   })
   
-### OCR ####################################################################
-  pngfile1 <- reactive({pdftools::pdf_convert(input$file_import$datapath,
+  ### OCR ####################################################################
+  pngfile1 <- reactive({pdftools::pdf_convert("0.pdf",
                                               dpi = 300)})
   text1 <- reactive({tesseract::ocr(pngfile1())})
   df1 = reactive({as_tibble(text1())})
@@ -62,45 +67,45 @@ server <- shinyServer(function(input, output, session) {
   # document type
   observeEvent(input$file_import,{
     updateSelectInput(session, "type", selected = 
-      if(paste(r1()[3,],r1()[4,],r1()[5,])==
-                  "Label Specification Approval")
-    {"Label Specification Approval Form (ie variable text approval)"}else{""})
+                        if(paste(r1()[3,],r1()[4,],r1()[5,])==
+                           "Label Specification Approval")
+                        {"Label Specification Approval Form (ie variable text approval)"}else{""})
   })
   
   # trial number
   observeEvent(input$file_import,{
     updateTextInput(session, "trial_number", value = 
-                  as.character(r1()[which(grepl("Trial", r1()[["word"]]))+2,]))
+                      as.character(r1()[which(grepl("Trial", r1()[["word"]]))+2,]))
   })
   
   # spec id
   observeEvent(input$file_import,{
     updateTextInput(session, "spec_id", value = 
-      paste(as.character(r1()[which(grepl("Spec", r1()[["word"]]))[2]+2,]),
-            "-",
-            as.character(r1()[which(grepl("Spec", r1()[["word"]]))[2]+3,]),
-            sep=""
-      )
+                      paste(as.character(r1()[which(grepl("Spec", r1()[["word"]]))[2]+2,]),
+                            "-",
+                            as.character(r1()[which(grepl("Spec", r1()[["word"]]))[2]+3,]),
+                            sep=""
+                      )
     )
   })
   
   # lot number
   observeEvent(input$file_import,{
     updateTextInput(session, "lot_number", value = 
-      as.character(r1()[which(grepl("Lot", r1()[["word"]]))+2,])
+                      as.character(r1()[which(grepl("Lot", r1()[["word"]]))+2,])
     )
   })
   
   # date
   observeEvent(input$file_import,{
     updateTextInput(session, "date", value = 
-      paste(as.character(r1()[which(grepl("QA", r1()[["word"]]))[2]+1,]),
-            "-",
-            as.character(r1()[which(grepl("QA", r1()[["word"]]))[2]+2,]),
-            "-",
-            as.character(r1()[which(grepl("QA", r1()[["word"]]))[2]+3,]),
-            sep=""
-            )
+                      paste(as.character(r1()[which(grepl("QA", r1()[["word"]]))[2]+1,]),
+                            "-",
+                            as.character(r1()[which(grepl("QA", r1()[["word"]]))[2]+2,]),
+                            "-",
+                            as.character(r1()[which(grepl("QA", r1()[["word"]]))[2]+3,]),
+                            sep=""
+                      )
     )
   })
   
@@ -112,13 +117,15 @@ server <- shinyServer(function(input, output, session) {
                         } else{"Kit"})
   })
   
-
-### To be deleted #############################################################  
+  
+  ### To be deleted #############################################################  
   # put the critical info into the dataframe
   observeEvent(input$file_import,{
+    output$feedback = renderText("")
+    
     df2 = reactive({
       tibble(
-        pdf_name = input$file_import$name,
+        pdf_name = "0.pdf",
         protocol = input$trial_number,
         report_type = input$type,
         label_spec_id = input$spec_id,
@@ -143,13 +150,15 @@ server <- shinyServer(function(input, output, session) {
     })
   })
   
-###############################################################################
+  ###############################################################################
   
   
   observeEvent(input$approve, {
+    output$feedback = renderText("Oh Yeah!")
+    
     df2 = reactive({
       tibble(
-        pdf_name = input$file_import$name,
+        pdf_name = "0.pdf",
         protocol = input$trial_number,
         report_type = input$type,
         label_spec_id = input$spec_id,
@@ -161,16 +170,16 @@ server <- shinyServer(function(input, output, session) {
     
     df3 = reactive({
       mutate(df2(), pdf_name=paste(
-      df2()[["protocol"]],"Label Specification Approval",
-      df2()[["label_spec_id"]],df2()[["pmd_lot_number"]],
-      df2()[["vial_kit"]],df2()[["date"]],
-      ".pdf"
-    ))
+        df2()[["protocol"]],"Label Specification Approval",
+        df2()[["label_spec_id"]],df2()[["pmd_lot_number"]],
+        df2()[["vial_kit"]],df2()[["date"]],
+        ".pdf"
+      ))
     })
     
-    file.rename(input$file_import$datapath,
+    file.rename("0.pdf",
                 df3()[["pdf_name"]]
-                )
+    )
     file.copy(df3()[["pdf_name"]], "final")
     
     # append to csv
@@ -181,15 +190,82 @@ server <- shinyServer(function(input, output, session) {
     
     
   })
+  
+  
+  observeEvent(input$next_pdf, {
+
+
+    
+    file.rename(input$file_import$datapath[x()], "0.pdf")
+    file.copy("0.pdf","www", overwrite = T)
+    
+    output$pdfview <- renderUI({
+      tags$iframe(style="height:1200px; width:100%", src="0.pdf")
+    })
+    
+    pngfile1 <- pdftools::pdf_convert("0.pdf",dpi = 300)
+    text1 <- tesseract::ocr(pngfile1)
+    df1 = as_tibble(text1)
+    r1 = df1 %>%
+        unnest_tokens(word, value, to_lower = F)
     
     
-   
+    # document type
+    updateSelectInput(session, "type", selected = 
+                        if(paste(r1[3,],r1[4,],r1[5,])==
+                           "Label Specification Approval")
+                        {"Label Specification Approval Form (ie variable text approval)"}else{""})
+    
+    
+    # trial number
+    updateTextInput(session, "trial_number", value = 
+                      as.character(r1[which(grepl("Trial", r1[["word"]]))+2,]))
+    
+    
+    # spec id
+    updateTextInput(session, "spec_id", value = 
+                      paste(as.character(r1[which(grepl("Spec", r1[["word"]]))[2]+2,]),
+                            "-",
+                            as.character(r1[which(grepl("Spec", r1[["word"]]))[2]+3,]),
+                            sep=""
+                      )
+    )
+    
+    
+    # lot number
+    updateTextInput(session, "lot_number", value = 
+                      as.character(r1[which(grepl("Lot", r1[["word"]]))+2,])
+    )
+    
+    
+    # date
+    updateTextInput(session, "date", value = 
+                      paste(as.character(r1[which(grepl("QA", r1[["word"]]))[2]+1,]),
+                            "-",
+                            as.character(r1[which(grepl("QA", r1[["word"]]))[2]+2,]),
+                            "-",
+                            as.character(r1[which(grepl("QA", r1[["word"]]))[2]+3,]),
+                            sep=""
+                      )
+    )
+    
+    
+    # vial or kit
+    updateSelectInput(session, "vial_kit", selected = 
+                        if(length(which(grepl("PRIMARY", r1[["word"]])))==1){
+                          "Vial"
+                        } else{"Kit"})
+    
+
+    
+ 
+    
+    
+  })
+  
+  
+  
   
 })
 
 shinyApp(ui = ui, server = server)
-
-
-
-
-
